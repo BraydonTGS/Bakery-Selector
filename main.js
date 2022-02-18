d836dec2c048a93bafb8c9994b2b64a022027fd8
Braydon <99298723+BraydonTGS@users.noreply.github.com>
2022-02-17 21:04:58 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log('Random Bakery Selector!!!');
+
+let randomNumber = Math.floor(Math.random()* 8);
+
+if (randomNumber === 0) {
+    console.log('Du Pain et Des Idees');
+
+} else if (randomNumber === 1) {
+    console.log('Utopie');
+
+} else if (randomNumber === 2) {
+    console.log('French Bastards');
+
+} else if(randomNumber === 3) {
+    console.log('Stohrer');
+
+} else if (randomNumber === 4) {
+    console.log('Le Petit Grain')
+
+} else if (randomNumber === 5) {
+    console.log('Boulangerie Poilane');
+
+} else if (randomNumber === 6) {
+    console.log('Mamiche');
+
+} else if (randomNumber === 7) {
+    console.log('Bontemps');
+
+} else if (randomNumber === 8) {
+    console.log('Laduree');
+
+} else {
+    console.log('Today is not your day');
+}
+
